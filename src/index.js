fe537e24b526551fe67ac3a25a0ac9b2be0e6572
matteda ramesh <mattedaramesh27@gmail.com>
2025-06-30 16:21:05 +0530

    used version 16

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-
+const rootApp = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootApp
+);
